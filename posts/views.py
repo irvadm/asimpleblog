239d500310824fff85f83de90cc20bc13b27c6c9
irvadm <amiam89@gmail.com>
2018-12-15 14:10:10 -0500

    Add post list URL and view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Post
+
+
+def posts(request):
+    return HttpResponse('<h1>Post List</h1>')
